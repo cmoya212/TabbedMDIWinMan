5a3294bc5fdc48923092d2d86f007898c9cd8ff5
Carlos Moya <cmoya@yahoo.com>
2024-06-15 16:42:33 -0400

    Initial commit

---
+++ MDIWindowManager Test/Form1.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.WindowManagerPanel1 = New MDIWindowManager.WindowManagerPanel()
+        Me.SuspendLayout()
+        '
+        'WindowManagerPanel1
+        '
+        Me.WindowManagerPanel1.AllowUserVerticalRepositioning = False
+        Me.WindowManagerPanel1.AutoDetectMdiChildWindows = True
+        Me.WindowManagerPanel1.AutoHide = False
+        Me.WindowManagerPanel1.ButtonRenderMode = MDIWindowManager.ButtonRenderMode.Standard
+        Me.WindowManagerPanel1.DisableCloseAction = False
+        Me.WindowManagerPanel1.DisableHTileAction = False
+        Me.WindowManagerPanel1.DisablePopoutAction = False
+        Me.WindowManagerPanel1.DisableTileAction = False
+        Me.WindowManagerPanel1.EnableAuroGlassEffect = True
+        Me.WindowManagerPanel1.EnableTabPaintEvent = False
+        Me.WindowManagerPanel1.Location = New System.Drawing.Point(2, 2)
+        Me.WindowManagerPanel1.MinMode = False
+        Me.WindowManagerPanel1.Name = "WindowManagerPanel1"
+        Me.WindowManagerPanel1.Orientation = MDIWindowManager.WindowManagerOrientation.Top
+        Me.WindowManagerPanel1.ShowCloseButton = True
+        Me.WindowManagerPanel1.ShowIcons = True
+        Me.WindowManagerPanel1.ShowLayoutButtons = True
+        Me.WindowManagerPanel1.ShowTitle = False
+        Me.WindowManagerPanel1.Size = New System.Drawing.Size(862, 35)
+        Me.WindowManagerPanel1.Style = MDIWindowManager.TabStyle.ClassicTabs
+        Me.WindowManagerPanel1.TabIndex = 0
+        Me.WindowManagerPanel1.TabRenderMode = MDIWindowManager.TabsProvider.Standard
+        Me.WindowManagerPanel1.Text = "WindowManagerPanel1"
+        Me.WindowManagerPanel1.TitleBackColor = System.Drawing.SystemColors.ControlDark
+        Me.WindowManagerPanel1.TitleForeColor = System.Drawing.SystemColors.ControlLightLight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(866, 622)
+        Me.Controls.Add(Me.WindowManagerPanel1)
+        Me.IsMdiContainer = True
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents WindowManagerPanel1 As MDIWindowManager.WindowManagerPanel
+End Class
---
+++ MDIWindowManager Test/WinAPI.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class WinAPI
+
+    <StructLayout(LayoutKind.Sequential)> _
+    Public Structure MARGINS
+        Public leftWidth As Integer
+        Public rightWidth As Integer
+        Public topHeight As Integer
+        Public bottomHeight As Integer
+    End Structure
+
+    <DllImport("dwmapi.dll")> _
+    Private Shared Function DwmExtendFrameIntoClientArea(ByVal hwnd As IntPtr, ByRef margins As MARGINS) As Integer
+    End Function
+
+    Private Declare Function DwmIsCompositionEnabled Lib "dwmapi" (ByRef pfEnabled As Long) As Long
+
+
+    Public Shared Sub SetAeroGlass(ctl As Control)
+
+        If IsSystemAeroGlassEnabled() Then
+            ctl.BackColor = Color.Black
+            Dim margins As New MARGINS()
+
+            margins.leftWidth = -1
+            margins.rightWidth = -1
+            margins.topHeight = -1
+            margins.bottomHeight = -1
+
+            DwmExtendFrameIntoClientArea(ctl.Handle, margins)
+
+        End If
+
+    End Sub
+
+    Public Shared Function IsSystemAeroGlassEnabled() As Boolean
+
+        Dim glassState As Long
+
+        Try
+            DwmIsCompositionEnabled(glassState)
+        Catch
+            glassState = 0
+        End Try
+
+        Return CBool(glassState)
+
+
+    End Function
+
+
+End Class
