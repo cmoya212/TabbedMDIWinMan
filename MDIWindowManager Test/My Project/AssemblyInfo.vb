d66d22e41eaf014dd2f3e36333ce9e57f07a32ca
Carlos Moya <cmoya@yahoo.com>
2024-06-16 12:26:21 -0400

    Fixed test project upgrade compatibility

--- MDIWindowManager Test/My Project/AssemblyInfo.vb
+++ MDIWindowManager Test/My Project/AssemblyInfo.vb
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("f74dcee9-2130-4f6f-83b2-1a34cf58aa3f")> 
+<Assembly: Guid("36892B5B-3598-417E-96F6-0D7A674EE0E2")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("1.0.1.0")>
+<Assembly: AssemblyFileVersion("1.0.1.0")>
